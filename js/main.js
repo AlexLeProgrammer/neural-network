913c06a1fc26163c1109c59f992e2a0ea44ab342
AlexLeProgrammer <alex.etienne@divtec.ch>
2024-01-30 12:57:32 +0100

    End of the default network.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 /**
- * Neural - network main script
+ * Neural - network | main script
  */
 
 "use strict";
+
+let nn = new NeuralNetwork([2, 1]);
+console.log(nn.out([1, 1]));
+
+for (let i = 0; i < 50; i++) {
+    nn.mutate(-1, 1)
+    console.log(nn.out([1, 1]));
+}
